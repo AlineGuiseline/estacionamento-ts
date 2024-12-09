a124e5ef8e2aa3e83787f9d0b593c478cc9126f3
Aline Guiseline <alineguiseline@gmail.com>
2024-12-09 09:22:18 -0300

    compiles date config

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 (function () {
-    const $ = (query) => document.querySelector(query); // para não precisar ficar criando querySelector 
+    var $ = function (query) {
+        return document.querySelector(query);
+    }; // para não precisar ficar criando querySelector 
     // para tudo
     function calcTempo(mil) {
-        const min = Math.floor(mil / 60000);
-        const sec = Math.floor((mil % 60000) / 1000);
-        return `${min}m e ${sec}s`;
+        var min = Math.floor(mil / 60000);
+        var sec = Math.floor((mil % 60000) / 1000);
+        return "".concat(min, "m e ").concat(sec, "s");
     }
     function patio() {
         function ler() {
@@ -15,9 +26,9 @@
         }
         function adicionar(veiculo, salva) {
             var _a, _b;
-            const row = document.createElement("tr");
+            var row = document.createElement("tr");
             // Formatar a data para exibição no HTML
-            const entradaFormatada = new Date(veiculo.entrada).toLocaleString('pt-BR', {
+            var entradaFormatada = new Date(veiculo.entrada).toLocaleString('pt-BR', {
                 year: '2-digit',
                 month: '2-digit',
                 day: '2-digit',
@@ -25,29 +36,22 @@
                 minute: '2-digit',
                 second: '2-digit',
             });
-            row.innerHTML = `
-                <td>${veiculo.nome}</td>
-                <td>${veiculo.placa}</td>
-                <td>${entradaFormatada}</td>
-                <td>
-                    <button class="delete" data-placa="${veiculo.placa}">X</button>
-                </td>
-            `;
+            row.innerHTML = "\n                <td>".concat(veiculo.nome, "</td>\n                <td>").concat(veiculo.placa, "</td>\n                <td>").concat(entradaFormatada, "</td>\n                <td>\n                    <button class=\"delete\" data-placa=\"").concat(veiculo.placa, "\">X</button>\n                </td>\n            ");
             (_a = row.querySelector(".delete")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
                 remover(this.dataset.placa);
             });
             (_b = $("#patio")) === null || _b === void 0 ? void 0 : _b.appendChild(row);
             if (salva)
-                salvar([...ler(), veiculo]);
+                salvar(__spreadArray(__spreadArray([], ler(), true), [veiculo], false));
         }
         function remover(placa) {
-            const veiculo = ler().find((veiculo) => veiculo.placa === placa);
+            var veiculo = ler().find(function (veiculo) { return veiculo.placa === placa; });
             if (!veiculo)
                 return;
-            const tempo = calcTempo(new Date().getTime() - new Date(veiculo.entrada).getTime());
-            if (!confirm(`O veículo ${veiculo.nome} permaneceu por ${tempo}. Deseja encerrar?`))
+            var tempo = calcTempo(new Date().getTime() - new Date(veiculo.entrada).getTime());
+            if (!confirm("O ve\u00EDculo ".concat(veiculo.nome, " permaneceu por ").concat(tempo, ". Deseja encerrar?")))
                 return;
-            salvar(ler().filter((veiculo) => veiculo.placa !== placa));
+            salvar(ler().filter(function (veiculo) { return veiculo.placa !== placa; }));
             renderizar();
         }
         function renderizar() {
@@ -55,26 +59,26 @@
             // o ! se chama "force". Como eu tenho certeza de que "patio" existe no
             // HTML, estou forçando o innerHTML a renderizá-lo sempre (é perigoso usar
             // o force. Use com moderação)
-            const patio = ler();
+            var patio = ler();
             if (patio.length) {
-                patio.forEach((veiculo) => adicionar(veiculo));
+                patio.forEach(function (veiculo) { return adicionar(veiculo); });
             }
         }
-        return { ler, adicionar, remover, salvar, renderizar };
+        return { ler: ler, adicionar: adicionar, remover: remover, salvar: salvar, renderizar: renderizar };
     }
     patio().renderizar();
-    const btnCadastrar = $("#cadastrar");
+    var btnCadastrar = $("#cadastrar");
     if (btnCadastrar) {
-        btnCadastrar.addEventListener("click", () => {
+        btnCadastrar.addEventListener("click", function () {
             var _a, _b;
-            const nome = (_a = $("#nome")) === null || _a === void 0 ? void 0 : _a.value;
-            const placa = (_b = $("#placa")) === null || _b === void 0 ? void 0 : _b.value;
+            var nome = (_a = $("#nome")) === null || _a === void 0 ? void 0 : _a.value;
+            var placa = (_b = $("#placa")) === null || _b === void 0 ? void 0 : _b.value;
             if (!nome || !placa) {
                 alert("Os campos nome e placa são obrigatórios");
                 return;
             }
-            const entrada = new Date().toISOString(); // Armazena no formato ISO
-            patio().adicionar({ nome, placa, entrada }, true);
+            var entrada = new Date().toISOString(); // Armazena no formato ISO
+            patio().adicionar({ nome: nome, placa: placa, entrada: entrada }, true);
         });
     }
     else {
